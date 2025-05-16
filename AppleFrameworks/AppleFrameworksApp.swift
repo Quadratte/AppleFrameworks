a0c2f2ff2716811ff3f0c38175979216242d8980
Sergey Philippov <quadratte@gmail.com>
2025-05-16 17:28:15 +0700

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Quadratte on 16.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
